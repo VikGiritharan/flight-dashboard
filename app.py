7466c3cc0ae4be0f5b9253becee5c14f9dec0a88
Vikash Giritharan <123036113+VikGiritharan@users.noreply.github.com>
2025-05-07 14:16:59 +0000

    Update app.py

--- app.py
+++ app.py
@@ -104,5 +104,8 @@ def predict(month, carrier, airport):
         disclaimer
     ])
 
+import os
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
